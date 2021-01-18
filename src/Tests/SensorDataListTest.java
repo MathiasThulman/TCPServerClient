737ae9a601455cddb78994281d82d45dc929094a
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2021-01-18 09:09:31 +0100

    very late initial commit

--- src/Tests/SensorDataListTest.java
+++ src/Tests/SensorDataListTest.java
@@ -1,26 +1,19 @@
 package Tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+import sensorData.SensorData;
+import sensorData.SensorDataExchanger;
+import sensorData.SensorDataSender;
+
+import java.io.*;
+
 import static org.junit.Assert.*;
 
 public class SensorDataListTest {
 
-    @org.junit.Test
-    public void add() {
-    }
+    @Test
+    void validTest1(){
 
-    @org.junit.Test
-    public void get() {
-    }
-
-    @org.junit.Test
-    public void getAverage() {
-    }
-
-    @org.junit.Test
-    public void getLength() {
-    }
-
-    @org.junit.Test
-    public void getAverageOfSensor() {
     }
 }
