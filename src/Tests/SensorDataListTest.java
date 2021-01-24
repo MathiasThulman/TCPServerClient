f1fb5d9ad8dcbfcdf2aa7d75d6b6be7ef5597b07
MathiasThulman <72448153+MathiasThulman@users.noreply.github.com>
2021-01-24 11:03:28 +0100

    added test method to get InputStream and OutPutstream fast

--- src/Tests/SensorDataListTest.java
+++ src/Tests/SensorDataListTest.java
@@ -5,12 +5,25 @@ import org.junit.Test;
 import sensorData.SensorData;
 import sensorData.SensorDataExchanger;
 import sensorData.SensorDataSender;
+import tcp.Connection;
+import tcp.TCPConnector;
 
 import java.io.*;
+import java.net.Socket;
 
 import static org.junit.Assert.*;
 
 public class SensorDataListTest {
+
+    public InputStream getInputStream() throws IOException {
+        Connection c = new TCPConnector();
+        return c.getInputStream();
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+        Connection c = new TCPConnector();
+        return c.getOutPutStream();
+    }
 
     @Test
     void validTest1(){
